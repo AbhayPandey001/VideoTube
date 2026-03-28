e32b37e23148fb4a5e5ecd7968af88e68f2c595e
Abhay Pandey <vasuabhaypandey2005@gmail.com>
2026-03-28 19:03:55 +0530

    complete dashboard controllers

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -14,9 +14,63 @@ const getChannelStats = asyncHandler(async (req, res) => {
         throw new ApiError(400, 'Invalid channel id')
     }
 
-    
+    const totalChannelViews = await Video.aggregate([
+        {
+            $match: {
+                owner: new mongoose.Types.ObjectId(channelId)
+            }
+        },
+        {
+            $group: {
+                _id: null,
+                totalViews: { $sum: "$views" }
+            }
+        }
+    ])
 
+    const totalViews = totalChannelViews[0]?.totalViews || 0
 
+    // counting of subscribers
+    const channelSubscribers = await Subscription.countDocuments({ channel: channelId })
+
+    // counting total videos
+    const totalChannelVideos = await Video.countDocuments({ owner: channelId })
+
+    // counting total likes
+    const totalChannelLikes = await Like.aggregate([
+        {
+            $lookup: {
+                from: "videos",
+                localField: "video",
+                foreignField: "_id",
+                as: 'likedVideoDetails'
+            }
+        },
+        {
+            $unwind: "$likedVideoDetails"
+        },
+        {
+            $match: {
+                "$likedVideoDetails.owner": new mongoose.Types.ObjectId(channelId)
+            }
+        },
+        {
+            $count: "totalLikes"
+        }
+    ])
+
+    const totalLikes = totalChannelLikes[0]?.totalLikes || 0
+
+    const stats = {
+        totalViews ,
+        totalSubscribers : channelSubscribers , 
+        totalVideos : totalChannelVideos ,
+        totalLikes
+    }
+
+    return res 
+        .status(200)
+        .json( new ApiResponse(200 , stats , 'Channel Stats fetched succesfully'))
 })
 
 const getChannelVideos = asyncHandler(async (req, res) => {
