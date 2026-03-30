189061127689c21b2d7f6632c415ec78866c7b81
Abhay Pandey <vasuabhaypandey2005@gmail.com>
2026-03-30 21:27:13 +0530

    fix subscription feature

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,4 +1,6 @@
 import mongoose , {Schema} from 'mongoose'
+import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2'
+
 
 const subscriptionSchema = new Schema({
     // id toh khud hi aa jati hai
@@ -12,4 +14,6 @@ const subscriptionSchema = new Schema({
     } 
 },{timestamps : true} )
 
+
+subscriptionSchema.plugin(mongooseAggregatePaginate)
 export const Subscription =  mongoose.model("Subscription" , subscriptionSchema)
